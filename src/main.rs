28169d051215351503e81a4f66f66413ad447d8f
Arpith Siromoney <arpith@feedreader.co>
2016-08-02 10:24:06 -0400

    read response body

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate hyper;
 
 use std::env;
+use std::io::Read;
 
 fn main() {
     println!("Going to make get request");
     let client = hyper::Client::new();
     if let Some(arg1) = env::args().nth(1) {
         let hostname = arg1;
-        let res = client.get(&hostname).send().unwrap();
+        let mut res = client.get(&hostname).send().unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
         assert_eq!(res.status, hyper::Ok);
         println!("Status: {}", res.status);
+        println!("Body: {}", body);
     }
 }
