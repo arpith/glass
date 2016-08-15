9cc6f2f8e47a399d5e01a879a63aa42521660d90
Arpith Siromoney <arpith@feedreader.co>
2016-08-15 12:26:18 -0400

    read title from page

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,44 @@ use std::string::String;
 use tendril::*;
 
 use html5ever::parse_document;
-use html5ever::rcdom::{Element, RcDom, Handle};
+use html5ever::rcdom::{Element, RcDom, Handle, Text};
 
 mod layout;
 
 pub fn escape_default(s: &str) -> String {
     s.chars().flat_map(|c| c.escape_default()).collect()
+}
+
+fn get_title(handle: Handle) -> String {
+    let mut queue: Vec<Handle> = Vec::new();
+    queue.push(handle);
+    let mut is_title_child = false;
+    while queue.len() != 0 {
+        let handle = queue.remove(0);
+        let node = handle.borrow();
+        match node.node {
+            Element(ref name, _, _) => {
+                if !is_title_child {
+                    if name.local == string_cache::Atom::from("title") {
+                        queue = Vec::new();
+                        is_title_child = true;
+                    }
+                }
+            },
+            Text(ref text) => {
+                if is_title_child {
+                    return String::from(text.clone());
+                }
+            },
+             _ => {
+                //don't do anything
+            }
+        }
+        for child in node.children.iter() {
+            queue.push(child.clone());
+        }
+    }
+    return String::from("");
 }
 
 fn get_css_links(handle: Handle) -> Vec<String> {
@@ -65,14 +97,14 @@ fn main() {
         println!("Status: {}", res.status);
         let dom = parse_document(RcDom::default(), Default::default()).from_utf8().read_from(&mut res).unwrap();
         println!("Parsed dom!");
-        /*
-        let csslinks = get_css_links(dom.document);
+        let csslinks = get_css_links(dom.document.clone());
         println!("CSS links: {:?}", csslinks);
-        for link in csslinks.iter() {
+/*        for link in csslinks.iter() {
             let res = client.get(link).send().unwrap();
             println!("Status for {}: {}", link, res.status);
         }
         */
-        layout::render(hostname, dom.document);
+        let title = get_title(dom.document.clone());
+        layout::render(title, dom.document);
     }
 }
